b8378dc30492272a58072b83da77260d4f1aaf6f
Steven Oliva <steven.oliva@hermes.com.pe>
2021-09-22 20:21:33 -0500

    El tercer commit

---
+++ src/main/java/com/oliva/operaciones/PruebaController.java
@@ -0,0 +1,12 @@
+package com.oliva.operaciones;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController	
+public class PruebaController {
+	@RequestMapping(value="/hola")
+	public String suma() {
+		return "Hola Mundo";
+	}
+}
